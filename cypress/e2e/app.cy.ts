bf9d192677bac643e6434a8b42fa300f8260b5b6
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-07 11:31:28 -0600

    feat: add multi-step form functionality

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -8,8 +8,20 @@ describe("general app test", () => {
   it("should display a form with inputs for the name and the email", () => {
     cy.visit("/");
 
-    cy.get("form").should("exist");
+    cy.get("form[data-cy='step-1-form'").should("exist");
     cy.get("input[name=name]").should("exist");
     cy.get("input[name=email]").should("exist");
+  });
+
+  it("should display the second form when the first form is submitted", () => {
+    cy.visit("/");
+
+    cy.get("input[name=name]").should("exist");
+    cy.get("input[name=email]").should("exist");
+
+    cy.get("form[data-cy='step-1-form'").submit();
+
+    cy.get("form[data-cy='step-1-form'").should("have.css", "display", "none");
+    cy.get("form[data-cy='step-2-form'").should("have.css", "display", "block");
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
 import "./style.css";
 
-console.log("Hello World!");
+const $ = document.querySelector.bind(document);
+
+const formStep1 = $(".step-1-form") as HTMLFormElement;
+const formStep2 = $(".step-2-form") as HTMLFormElement;
+
+changeStep(1);
+
+formStep1.addEventListener("submit", (e: SubmitEvent) => {
+  e.preventDefault();
+
+  changeStep(2);
+});
+
+function changeStep(step: number) {
+  switch (step) {
+    case 1:
+      formStep1.classList.remove("hidden");
+      formStep2.classList.add("hidden");
+      break;
+    case 2:
+      formStep1.classList.add("hidden");
+      formStep2.classList.remove("hidden");
+      break;
+  }
+}
